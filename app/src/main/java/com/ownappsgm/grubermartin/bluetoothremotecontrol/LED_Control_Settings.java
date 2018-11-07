3e3f3ee9cc57403a90476f91e4bf4a625c5ae5dd
Martin Gruber <martin.gruber@stud.hslu.ch>
2018-11-07 09:00:39 +0100

    EditText angepasst

--- app/src/main/java/com/ownappsgm/grubermartin/bluetoothremotecontrol/LED_Control_Settings.java
+++ app/src/main/java/com/ownappsgm/grubermartin/bluetoothremotecontrol/LED_Control_Settings.java
@@ -1,35 +1,42 @@
 package com.ownappsgm.grubermartin.bluetoothremotecontrol;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LED_Control_Settings extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener {
+public class LED_Control_Settings extends AppCompatActivity implements View.OnClickListener, TextView.OnEditorActionListener, View.OnTouchListener {
 
     TextView tvUserHintLedSettings;
     Button btnLedOnSetting, btnLedOffSetting, btnSaveLedSettings;
     EditText etLedOnCommand, etLedOffCommand;
+    ConstraintLayout led_control_constraint_layout;
 
     BluetoothDevice savedBTdevice;
 
     final String PrefBtnCommandOnKey = "btnCommandOnKey";
     final String PrefBtnCommandOffKey = "btnCommandOffKey";
     Boolean testBool;
+    Boolean onTouchedCalled;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_led__control__settings);
+        led_control_constraint_layout = (ConstraintLayout) findViewById(R.id.led_control_constraint_layout);
         tvUserHintLedSettings = (TextView) findViewById(R.id.tvUserHintLedSettings);
         btnLedOnSetting = (Button) findViewById(R.id.btnLedOnSetting);
         btnLedOffSetting = (Button) findViewById(R.id.btnLedOffSetting);
@@ -42,28 +49,67 @@ public class LED_Control_Settings extends AppCompatActivity implements View.OnCl
         etLedOffCommand.setOnClickListener(this);
         etLedOnCommand.setOnEditorActionListener(this);
         etLedOffCommand.setOnEditorActionListener(this);
+        etLedOnCommand.setOnTouchListener(this);
+        etLedOffCommand.setOnTouchListener(this);
         etLedOnCommand.setOnClickListener(this);
         btnSaveLedSettings.setOnClickListener(this);
         etLedOnCommand.setCursorVisible(false);
         etLedOffCommand.setCursorVisible(false);
+        etLedOffCommand.setFocusableInTouchMode(true);
+        etLedOnCommand.setFocusableInTouchMode(true);
+        led_control_constraint_layout.setOnTouchListener(this);
 
         savedBTdevice = getIntent().getExtras().getParcelable("saveCurrentDevice");
     }
 
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        onTouchedCalled = false;
+        switch(v.getId())
+        {
+            case R.id.led_control_constraint_layout:
+                hideKeyboard(this);
+                etLedOffCommand.clearFocus();
+                etLedOnCommand.clearFocus();
+                etLedOnCommand.setCursorVisible(false);
+                etLedOffCommand.setCursorVisible(false);
+                break;
+
+            case R.id.etLedOffCommand:
+                etLedOffCommand.requestFocus();
+                etLedOffCommand.setCursorVisible(true);
+                etLedOffCommand.setSelection(etLedOffCommand.getText().length());
+                break;
+
+            case R.id.etLedOnCommand:
+                etLedOnCommand.requestFocus();
+                etLedOnCommand.setCursorVisible(true);
+                etLedOnCommand.setSelection(etLedOnCommand.getText().length());
+                break;
+        }
+        return onTouchedCalled;
+    }
 
     @Override
     public void onClick(View v) {
         switch(v.getId())
         {
             case R.id.etLedOffCommand:
+//                etLedOffCommand.setCursorVisible(true);
+                etLedOffCommand.requestFocus();
                 etLedOffCommand.setCursorVisible(true);
-                etLedOffCommand.setFocusable(true);
+                etLedOffCommand.setSelection(etLedOffCommand.getText().length());
                 break;
             case R.id.etLedOnCommand:
+//                etLedOnCommand.setCursorVisible(true);
+                etLedOnCommand.requestFocus();
                 etLedOnCommand.setCursorVisible(true);
-                etLedOnCommand.setFocusable(true);
+                etLedOnCommand.setSelection(etLedOnCommand.getText().length());
                 break;
             case R.id.btnSaveLedSettings:
+                hideKeyboard(this);
                 final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 SharedPreferences.Editor editor = prefs.edit();
                 editor.putString(PrefBtnCommandOnKey,etLedOnCommand.getText().toString());
@@ -82,21 +128,23 @@ public class LED_Control_Settings extends AppCompatActivity implements View.OnCl
         boolean handled = false;
 
         if (actionId == EditorInfo.IME_ACTION_NEXT) {
-            etLedOnCommand.setCursorVisible(false);
-            etLedOnCommand.setFocusable(false);
+            /*etLedOnCommand.setCursorVisible(false);
+            etLedOnCommand.setFocusable(true);
             etLedOffCommand.setCursorVisible(true);
-            etLedOffCommand.setFocusable(true);
+            etLedOffCommand.setFocusable(true);*/
+            etLedOffCommand.requestFocus();
+
 
             handled = true;
         }
 
         if(actionId == EditorInfo.IME_ACTION_DONE)
         {
-            etLedOnCommand.setCursorVisible(false);
+            /*etLedOnCommand.setCursorVisible(false);
             etLedOnCommand.setFocusable(false);
             etLedOffCommand.setCursorVisible(false);
             etLedOffCommand.setSelected(false);
-            etLedOffCommand.setFocusable(false);
+            etLedOffCommand.setFocusable(false);*/
             performClick(btnSaveLedSettings);
 
 
@@ -106,7 +154,20 @@ public class LED_Control_Settings extends AppCompatActivity implements View.OnCl
 
     }
 
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
     public static boolean performClick(View view) {
         return view.isEnabled() && view.isClickable() && view.performClick();
     }
+
+
 }
